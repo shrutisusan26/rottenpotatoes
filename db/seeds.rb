838d3c3b069acb282bfe75b8c9d3576d5cfd6ef2
Shrutisusan26 <shrutisusan055@gmail.com>
2023-09-15 14:37:44 -0500

    Added new movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Titanic', :rating => 'PG-13',
+  :release_date => '19-Dec-1997'},
+  {:title => 'A walk to remember', :rating => 'PG-13',
+  :release_date => '25-Jan-2002'},
+  {:title => 'The Notebook', :rating => 'PG-13',
+  :release_date => '19-Dec-2000'},
 ]
 
 more_movies.each do |movie|
